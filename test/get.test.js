27e9c5d11d1411451f0287e42252525a15df3ef8
Jonatan <jonatan.schmidlechner@tuni.fi>
2024-11-16 21:05:11 +0200

    Add, edit and refactor tests for get

--- test/get.test.js
+++ test/get.test.js
@@ -1,52 +1,58 @@
 import get from '../src/get.js';
-describe('get function', () => {
 
-  // Test for basic property access
+
+ describe('When using dot notation to access properties', () => {
   test('returns the value of a direct property', () => {
     const object = { a: 1 };
     expect(get(object, 'a')).toBe(1);
   });
 
-  // Test for nested property access
   test('returns the value of a nested property using dot notation', () => {
     const object = { a: { b: { c: 3 } } };
     expect(get(object, 'a.b.c')).toBe(3);
   });
 
-  // Test for accessing array elements
-  test('returns the value at the array index', () => {
-    const object = { a: [{ b: 2 }] };
-    expect(get(object, 'a[0].b')).toBe(2);
+  test('returns "undefined" in an array property', () => {
+    const object = { a: [{ b: undefined }] };
+    expect(get(object, 'a[0].b')).toEqual(undefined);
   });
 
+  test('returns the value in a nested array property, even if it is null', () => {
+    const object = { a: [[1, 2, { b: null }]] };
+    expect(get(object, 'a[0][2].b')).toBe(null);
+  });
+
+  test('returns the value at the array index using dot notation', () => {
+    const object = { a: [{ "0": 2 }] };
+    expect(get(object, 'a.0.0')).toBe(2);
+  });
+});
+
+describe('When using array path notation to access properties', () => {
   test('returns the value at the array index using array path notation', () => {
-    const object = { a: [{ b: 2 }] };
-    expect(get(object, ['a', '0', 'b'])).toBe(2);
+    const object = { a: [{ 0: 2 }] };
+    expect(get(object, ['a', '0', '0'])).toBe(2);
   });
+});
 
-  // Test for default value return
-  test('returns the default value when the property is undefined', () => {
-    const object = { a: { b: 2 } };
-    expect(get(object, 'a.c', 'default')).toBe('default');
-  });
-
+describe('When using a default value', () => {
   test('returns the default value when the property path does not exist', () => {
     const object = { a: { b: 2 } };
     expect(get(object, 'a.x.y', 'default')).toBe('default');
   });
 
-  // Test for null or undefined object
-  test('returns undefined when the object is null', () => {
-    const object = null;
-    expect(get(object, 'a')).toBeUndefined();
+  test('returns the default value when the property is undefined', () => {
+    const object = { a: { b: undefined } };
+    expect(get(object, 'a.b', "this is default value")).toBe("this is default value");
   });
 
-  test('returns undefined when the object is undefined', () => {
-    const object = undefined;
-    expect(get(object, 'a')).toBeUndefined();
+  test('returns the default even if it is an object, when found property is undefined', () => {
+    const object = { a: { b: undefined } };
+    expect(get(object, 'a.b', {a: 2})).toEqual({a: 2});
   });
+});
 
-  // Test for empty or malformed paths
+describe('When given invalid parameters and no default value', () => {
   test('returns undefined for an empty path', () => {
     const object = { a: 1 };
     expect(get(object, '')).toBeUndefined();
@@ -57,30 +63,64 @@ describe('get function', () => {
     expect(get(object, 'a[0]')).toBeUndefined();
   });
 
-  // Test for invalid paths and default fallback
-  test('returns default value for an invalid path with a default specified', () => {
-    const object = { a: 1 };
-    expect(get(object, 'a.b.c', 'default')).toBe('default');
+  test('returns undefined for an invalid path in array format', () => {
+    const object = { a: {b: 3} };
+    expect(get(object, ['a', 0, 'b', 'c'])).toBeUndefined();
+  });
+});
+
+describe('When given edge cases', () => {
+  test('Should return undefined for null', () => {
+    expect(get(null, 'a')).toBeUndefined();
   });
 
-  test('returns the default value for undefined path', () => {
-    const object = { a: { b: 2 } };
-    expect(get(object, 'a.x.y', 'default')).toBe('default');
+  test('Should return undefined for undefined', () => {
+    expect(get(undefined, 'a')).toBeUndefined();
   });
 
-  // Test for retrieving undefined from an object with undefined properties
-  test('returns default value when the property is explicitly undefined', () => {
-    const object = { a: undefined };
-    expect(get(object, 'a', 'default')).toBe('default');
+  test('Should return undefined for NaN', () => {
+    expect(get(NaN, 'a')).toBeUndefined();
   });
 
+  test('Should return undefined for Infinity', () => {
+    expect(get(Infinity, 'a')).toBeUndefined();
+  });
+
+  test('Should return undefined for string', () => {
+    expect(get('test', 'a')).toBeUndefined();
+  });
+
+  test('Should return undefined for number', () => {
+    expect(get(123, 'a')).toBeUndefined();
+  });
+
+  test('Should return undefined for boolean', () => {
+    expect(get(true, 'a')).toBeUndefined();
+  });
+
+  test('Should return undefined for symbol', () => {
+    expect(get(Symbol('a'), 'a')).toBeUndefined();
+  });
+
+  test('Should return undefined for array', () => {
+    expect(get([1, 2, 3], 'a')).toBeUndefined();
+  });
+
+  test('Should return undefined for function', () => {
+    expect(get(() => {}, 'a')).toBeUndefined();
+  });
+});
 
 
-  // Test for symbol paths
-  test('returns value for symbol path', () => {
-    const sym = Symbol('a');
-    const object = { [sym]: 1 };
-    expect(get(object, sym)).toBe(1);
+
+
+describe('When using symbols in path', () => {
+  test('returns value for symbol path in array format', () => {
+    const sym1 = Symbol('a');
+    const sym2 = Symbol('b');
+    const resultSym = Symbol('result');
+    const object = { [sym1]: {[sym2]: resultSym} };
+    expect(get(object, [sym1, sym2])).toBe(resultSym);
   });
 
   test('returns default value for invalid symbol path', () => {
@@ -88,15 +128,17 @@ describe('get function', () => {
     const object = {};
     expect(get(object, sym, 'default')).toBe('default');
   });
+});
 
-  // Test for memoized function
-  test('returns memoized value for repeated calls', () => {
-    const object = { a: { b: 2 } };
-    const memoizedGet = get(object, 'a.b');
-    expect(memoizedGet).toBe(2);
-    expect(get(object, 'a.b')).toBe(2); // Should hit the cache
+describe('When objects inherit properties', () => {
+  test('returns inherited property', () => {
+    const parent = { a: 1 };
+    const child = Object.create(parent);
+    expect(get(child, 'a')).toBe(1);
   });
+});
 
+describe('When object is modified after get', () => {
   test('returns new value after object modification', () => {
     const object = { a: { b: 2 } };
     expect(get(object, 'a.b')).toBe(2);
